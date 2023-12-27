71109b752039dc1fea644e9d6482e33810661369
Ross Brewster <ross.brewster14@gmail.com>
2023-12-27 19:12:55 +0000

    Setup loading animations with spinners npm package.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,28 @@
 import { useEffect, useState } from 'react';
+import BarLoader from 'react-spinners/BarLoader';
 import './App.css';
 
 export default function App() {
   const [serverData, setServerData] = useState('');
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     async function readServerData() {
-      setServerData('Loading...');
       const resp = await fetch('/api/test');
       const data = await resp.json();
 
       console.log('Data from server:', data);
-
       setServerData(data);
+      setLoading(false);
     }
-
     readServerData();
   }, []);
 
-  return <h1>{serverData}</h1>;
+  return (
+    <>
+      {loading && <BarLoader color="#5F9FFF" />}
+      {!loading && <h1>{serverData}</h1>}
+    </>
+  );
 }
